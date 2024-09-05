9091a2bc14636f38d65fc0b6c204640300c1271d
Sean Cheatham <seancheatham@gmail.com>
2024-09-05 09:15:57 -0400

    Docs CNAME

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://seancheatham.github.io',
+  url: 'https://docs.giraffechain.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/giraffe/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
