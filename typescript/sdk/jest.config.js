459e80314cfc65c4f37785997eb88e16cb29f902
Sean Cheatham <seancheatham@gmail.com>
2024-09-17 20:52:55 -0400

    SDK test fixes

--- typescript/sdk/jest.config.js
+++ typescript/sdk/jest.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
