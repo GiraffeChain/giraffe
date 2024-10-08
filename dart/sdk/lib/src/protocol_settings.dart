8e11743061921427cb9f2b6f0f03daf3110d41c9
Sean Cheatham <SeanCheatham@users.noreply.github.com>
2024-10-08 19:36:13 -0400

    Ouroboros (#41)
    * Fallback to Ouroboros parameters. Adjust defaults.
    
    * Update dart protocol settings

--- dart/sdk/lib/src/protocol_settings.dart
+++ dart/sdk/lib/src/protocol_settings.dart
@@ -3,32 +3,23 @@ import 'package:rational/rational.dart';
 
 class ProtocolSettings {
   final Rational fEffective;
-  final int vrfLddCutoff;
   final int vrfPrecision;
-  final Rational vrfBaselineDifficulty;
   final Rational vrfAmplitude;
-  final int vrfSlotGap;
   final int chainSelectionKLookback;
   final Duration slotDuration;
 
   ProtocolSettings({
     required this.fEffective,
-    required this.vrfLddCutoff,
     required this.vrfPrecision,
-    required this.vrfBaselineDifficulty,
     required this.vrfAmplitude,
-    required this.vrfSlotGap,
     required this.chainSelectionKLookback,
     required this.slotDuration,
   });
 
   factory ProtocolSettings.fromMap(Map<String, String> map) => ProtocolSettings(
         fEffective: _parseRational(map["f-effective"]!),
-        vrfLddCutoff: int.parse(map["vrf-ldd-cutoff"]!),
         vrfPrecision: int.parse(map["vrf-precision"]!),
-        vrfBaselineDifficulty: _parseRational(map["vrf-baseline-difficulty"]!),
         vrfAmplitude: _parseRational(map["vrf-amplitude"]!),
-        vrfSlotGap: int.parse(map["vrf-slot-gap"]!),
         chainSelectionKLookback: int.parse(map["chain-selection-k-lookback"]!),
         slotDuration:
             Duration(milliseconds: int.parse(map["slot-duration-ms"]!)),
@@ -38,21 +29,12 @@ class ProtocolSettings {
         fEffective: map.containsKey("f-effective")
             ? _parseRational(map["f-effective"]!)
             : fEffective,
-        vrfLddCutoff: map.containsKey("vrf-ldd-cutoff")
-            ? int.parse(map["vrf-ldd-cutoff"]!)
-            : vrfLddCutoff,
         vrfPrecision: map.containsKey("vrf-precision")
             ? int.parse(map["vrf-precision"]!)
             : vrfPrecision,
-        vrfBaselineDifficulty: map.containsKey("vrf-baseline-difficulty")
-            ? _parseRational(map["vrf-baseline-difficulty"]!)
-            : vrfBaselineDifficulty,
         vrfAmplitude: map.containsKey("vrf-amplitude")
             ? _parseRational(map["vrf-amplitude"]!)
             : vrfAmplitude,
-        vrfSlotGap: map.containsKey("vrf-slot-gap")
-            ? int.parse(map["vrf-slot-gap"]!)
-            : vrfSlotGap,
         chainSelectionKLookback: map.containsKey("chain-selection-k-lookback")
             ? int.parse(map["chain-selection-k-lookback"]!)
             : chainSelectionKLookback,
@@ -62,19 +44,16 @@ class ProtocolSettings {
       );
 
   static const defaultAsMap = {
-    "f-effective": "3/25",
-    "vrf-ldd-cutoff": "18",
+    "f-effective": "1/50",
     "vrf-precision": "40",
-    "vrf-baseline-difficulty": "1/20",
-    "vrf-amplitude": "6/15",
-    "vrf-slot-gap": "2",
+    "vrf-amplitude": "1/40",
     "chain-selection-k-lookback": "576",
-    "slot-duration-ms": "1000",
+    "slot-duration-ms": "500",
   };
 
   @override
   String toString() =>
-      "ProtocolSettings(fEffective=$fEffective, vrfLddCutoff=$vrfLddCutoff, vrfPrecision=$vrfPrecision, vrfBaselineDifficulty=$vrfBaselineDifficulty, vrfAmplitude=$vrfAmplitude, vrfSlotGap=$vrfSlotGap, kLookback=$chainSelectionKLookback slotDuration=${slotDuration.inMilliseconds}ms, epochLength=$epochLength)";
+      "ProtocolSettings(fEffective=$fEffective, vrfPrecision=$vrfPrecision, vrfAmplitude=$vrfAmplitude, kLookback=$chainSelectionKLookback slotDuration=${slotDuration.inMilliseconds}ms, epochLength=$epochLength)";
 
   static final ProtocolSettings defaultSettings =
       ProtocolSettings.fromMap(defaultAsMap);
