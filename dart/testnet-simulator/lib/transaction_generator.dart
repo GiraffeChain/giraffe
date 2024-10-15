f6cd3c13816a570fac50e14f593d90b7ded6f42b
Sean Cheatham <seancheatham@gmail.com>
2024-10-15 09:46:12 -0400

    Update transaction generator throttling

--- dart/testnet-simulator/lib/transaction_generator.dart
+++ dart/testnet-simulator/lib/transaction_generator.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:fixnum/fixnum.dart';
 import 'package:giraffe_sdk/sdk.dart';
+import 'package:rxdart/transformers.dart';
 
 class TransactionGenerator {
   final List<Wallet> wallets;
@@ -13,8 +14,8 @@ class TransactionGenerator {
     required this.clients,
   });
 
-  Stream<Transaction> run(Duration period) =>
-      _targets(period).parAsyncMap(4, (t) async {
+  Stream<Transaction> run(Duration period) => _targets(period)
+      .parAsyncMap(16, (t) async {
         final (sender, recipient, client) = t;
         await sender.update(client);
         final transaction = await sender.payAndAttest(
@@ -28,31 +29,33 @@ class TransactionGenerator {
               ],
             ));
         return (transaction, client);
-      }).parAsyncMap(32, (t) async {
+      })
+      .throttleTime(period)
+      .parAsyncMap(32, (t) async {
         final (transaction, client) = t;
         await client.broadcastTransaction(transaction);
         return transaction;
       });
 
-  Stream<(Wallet, Wallet, BlockchainClient)> _targets(Duration period) =>
-      Stream.value(0).asyncExpand((walletIndex) {
-        _incIndex() => walletIndex = (walletIndex + 1) % wallets.length;
-        return Stream.periodic(period).asyncMap((_) async {
-          final client = clients[_random.nextInt(clients.length)];
-          Wallet sender = wallets[walletIndex];
-          await sender.update(client);
-          _incIndex();
-          while (sender.liquidFunds < Int64(4000)) {
-            sender = wallets[walletIndex];
-            await sender.update(client);
-            _incIndex();
-          }
-          var recipientIndex = _random.nextInt(wallets.length);
-          while (recipientIndex == walletIndex) {
-            recipientIndex = _random.nextInt(wallets.length);
-          }
-          final recipient = wallets[recipientIndex];
-          return (sender, recipient, client);
-        });
-      });
+  Stream<(Wallet, Wallet, BlockchainClient)> _targets(Duration period) async* {
+    var walletIndex = 0;
+    _incIndex() => walletIndex = (walletIndex + 1) % wallets.length;
+    while (true) {
+      final client = clients[_random.nextInt(clients.length)];
+      Wallet sender = wallets[walletIndex];
+      await sender.update(client);
+      _incIndex();
+      while (sender.liquidFunds < Int64(4000)) {
+        sender = wallets[walletIndex];
+        await sender.update(client);
+        _incIndex();
+      }
+      var recipientIndex = _random.nextInt(wallets.length);
+      while (recipientIndex == walletIndex) {
+        recipientIndex = _random.nextInt(wallets.length);
+      }
+      final recipient = wallets[recipientIndex];
+      yield (sender, recipient, client);
+    }
+  }
 }
